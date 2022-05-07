2e92f77cc26f9f476ffe6f396740c278093e1c9b
Sarah Fielding Smith <s.fieldingsmith@gmail.com>
2022-05-07 20:40:41 +0100

    render dummy api data

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,23 +1,35 @@
-export default function Question() {
+export default function Question(props) {
+  //   create answers array of objects {answer: text, correct: false}
+  const answers = props.incorrectAnswers.map((answer) => {
+    return { answer: answer, correct: false };
+  });
+
+  //   push correct answer into answers array
+  answers.push({ answer: props.correctAnswer, correct: true });
+
+  //   randomise array
+  for (let i = answers.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i);
+    let k = answers[i];
+    answers[i] = answers[j];
+    answers[j] = k;
+  }
+
+  //   destructure answers array
+  const [answer1, answer2, answer3, answer4] = answers;
+
   return (
     <div className="question__container">
-      <p className="question__title">How would one say goodbye in Spanish?</p>
+      <p className="question__title">{props.question}</p>
       <div className="answers__container">
-        <input type="radio" id="adios" name="questionID" />
-        <label for="adios">Adios</label>
-        <br />
-
-        <input type="radio" id="hola" name="questionID" />
-        <label for="hola">Hola</label>
-        <br />
-
-        <input type="radio" id="au-revoir" name="questionID" />
-        <label for="au-revoir">Au Revoir</label>
-        <br />
-
-        <input type="radio" id="salir" name="questionID" />
-        <label for="salir">Salir</label>
-        <br />
+        <input type="radio" id={answer1.answer} name={props.id} />
+        <label htmlFor={answer1.answer}>{answer1.answer}</label>
+        <input type="radio" id={answer2.answer} name={props.id} />
+        <label htmlFor={answer2.answer}>{answer2.answer}</label>
+        <input type="radio" id={answer3.answer} name={props.id} />
+        <label htmlFor={answer3.answer}>{answer3.answer}</label>
+        <input type="radio" id={answer4.answer} name={props.id} />
+        <label htmlFor={answer4.answer}>{answer4.answer}</label>
       </div>
     </div>
   );
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,13 +1,23 @@
 import Question from "./Question";
+import results from "../results";
 
 export default function Quiz() {
+  // map over results array creating new questionElements
+  const questionElements = results.map((result, index) => {
+    return (
+      <Question
+        question={result.question}
+        correctAnswer={result.correct_answer}
+        incorrectAnswers={result.incorrect_answers}
+        key={index}
+        id={index}
+      />
+    );
+  });
+
   return (
     <form className="quiz__container">
-      <Question />
-      <Question />
-      <Question />
-      <Question />
-      <Question />
+      {questionElements}
       <button className="btn btn__check">Check answers</button>
     </form>
   );
