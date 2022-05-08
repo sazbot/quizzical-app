59d3d7f72fadb5b5e1f4a5ed76bb4b01d7c204cd
Sarah Fielding Smith <s.fieldingsmith@gmail.com>
2022-05-08 16:32:52 +0100

    restore previous version

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,22 @@
 export default function Question(props) {
+  //   create answers array of objects {answer: text, correct: false}
+  const answers = props.incorrectAnswers.map((answer) => {
+    return { answer: answer, correct: false };
+  });
+
+  //   push correct answer into answers array
+  answers.push({ answer: props.correctAnswer, correct: true });
+
+  //   randomise array
+  for (let i = answers.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i);
+    let k = answers[i];
+    answers[i] = answers[j];
+    answers[j] = k;
+  }
+
   //   destructure answers array
-  const [answer1, answer2, answer3, answer4] = props.answers;
-  console.log(answer1);
+  const [answer1, answer2, answer3, answer4] = answers;
 
   return (
     <div className="question__container">
