65b4749b5117af833f543690de100e256cc6bb23
Sarah Fielding Smith <s.fieldingsmith@gmail.com>
2022-05-08 14:04:36 +0100

    set questions in state

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,19 +1,76 @@
+import React from "react";
 import Question from "./Question";
 import results from "../results";
+import { nanoid } from "nanoid";
 
 export default function Quiz() {
-  // map over results array creating new questionElements
-  const questionElements = results.map((result, index) => {
+  // map results to new question array
+  let questionsArray = results.map((result, index) => {
+    return {
+      question: result.question,
+      questionID: index,
+      answersArray: [
+        {
+          answer: result.correct_answer,
+          answerID: nanoid(),
+          isCorrect: true,
+          isSelected: false,
+        },
+        {
+          answer: result.incorrect_answers[0],
+          answerID: nanoid(),
+          isCorrect: false,
+          isSelected: false,
+        },
+        {
+          answer: result.incorrect_answers[1],
+          answerID: nanoid(),
+          isCorrect: false,
+          isSelected: false,
+        },
+        {
+          answer: result.incorrect_answers[2],
+          answerID: nanoid(),
+          isCorrect: false,
+          isSelected: false,
+        },
+      ],
+    };
+  });
+
+  // for each question, shuffle answersArray
+  questionsArray = questionsArray.map((question) => {
+    let shuffledArray = question.answersArray;
+
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * i);
+      let k = shuffledArray[i];
+      shuffledArray[i] = shuffledArray[j];
+      shuffledArray[j] = k;
+    }
+
+    return {
+      ...question,
+      answersArray: shuffledArray,
+    };
+  });
+
+  // set questionsArray in state
+  const [questions, setQuestions] = React.useState(questionsArray);
+
+  // map over questions creating question elements
+  const questionElements = questions.map((question) => {
     return (
       <Question
-        question={result.question}
-        correctAnswer={result.correct_answer}
-        incorrectAnswers={result.incorrect_answers}
-        key={index}
-        id={index}
+        question={question.question}
+        key={question.questionID}
+        id={question.questionID}
+        answers={question.answersArray}
       />
     );
   });
+
+  // create 'selectAnswer' function to send to each question (with answer id as parameter). Function should update array of questions objects held in state (update 'isSelected' property to true)
 
   return (
     <form className="quiz__container">
