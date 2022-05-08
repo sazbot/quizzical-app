4879d8a5b4b52b2f2b24195f1ce4e201cffa27e4
Sarah Fielding Smith <s.fieldingsmith@gmail.com>
2022-05-08 18:51:16 +0100

    handle change event when radio buttons selected

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,28 @@
+import React from "react";
+import { nanoid } from "nanoid";
+
 export default function Question(props) {
   //   create answers array of objects {answer: text, correct: false}
   const answers = props.incorrectAnswers.map((answer) => {
-    return { answer: answer, correct: false };
+    return {
+      questionID: props.questionID,
+      answer: answer,
+      answerID: nanoid(),
+      isCorrect: false,
+      isSelected: false,
+      answerSelected: "",
+    };
   });
 
   //   push correct answer into answers array
-  answers.push({ answer: props.correctAnswer, correct: true });
+  answers.push({
+    questionID: props.questionID,
+    answer: props.correctAnswer,
+    answerID: nanoid(),
+    isCorrect: true,
+    isSelected: false,
+    answerSelected: "",
+  });
 
   //   randomise array
   for (let i = answers.length - 1; i > 0; i--) {
@@ -15,20 +32,47 @@ export default function Question(props) {
     answers[j] = k;
   }
 
-  //   destructure answers array
+  // set answers array in state
+  const [answersArray, setAnswersArray] = React.useState(answers);
+
+  // //   destructure answers array
   const [answer1, answer2, answer3, answer4] = answers;
 
   return (
     <div className="question__container">
       <p className="question__title">{props.question}</p>
       <div className="answers__container">
-        <input type="radio" id={answer1.answer} name={props.id} />
+        <input
+          type="radio"
+          id={answer1.answer}
+          name={answer1.questionID}
+          value={answer1.answerID}
+          onChange={props.handleChange}
+        />
         <label htmlFor={answer1.answer}>{answer1.answer}</label>
-        <input type="radio" id={answer2.answer} name={props.id} />
+        <input
+          type="radio"
+          id={answer2.answer}
+          name={answer2.questionID}
+          value={answer2.answerID}
+          onChange={props.handleChange}
+        />
         <label htmlFor={answer2.answer}>{answer2.answer}</label>
-        <input type="radio" id={answer3.answer} name={props.id} />
+        <input
+          type="radio"
+          id={answer3.answer}
+          name={answer3.questionID}
+          value={answer3.answerID}
+          onChange={props.handleChange}
+        />
         <label htmlFor={answer3.answer}>{answer3.answer}</label>
-        <input type="radio" id={answer4.answer} name={props.id} />
+        <input
+          type="radio"
+          id={answer4.answer}
+          name={answer4.questionID}
+          value={answer4.answerID}
+          onChange={props.handleChange}
+        />
         <label htmlFor={answer4.answer}>{answer4.answer}</label>
       </div>
     </div>
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,7 +1,20 @@
+import React from "react";
 import Question from "./Question";
 import results from "../results";
 
 export default function Quiz() {
+  // map results to new question array
+  let questionArray = results.map((result, index) => {
+    return {
+      question: result.question,
+      questionID: index,
+      questionCorrect: false,
+    };
+  });
+
+  // set questions in state
+  const [questions, setQuestions] = React.useState(questionArray);
+
   // map over results array creating new questionElements
   const questionElements = results.map((result, index) => {
     return (
@@ -10,10 +23,26 @@ export default function Quiz() {
         correctAnswer={result.correct_answer}
         incorrectAnswers={result.incorrect_answers}
         key={index}
-        id={index}
+        questionID={index}
+        handleChange={selectAnswer}
       />
     );
   });
+
+  // create handleChange function to update isSelected value set in state
+  function selectAnswer(event) {
+    const { name, value, type, checked } = event.target;
+    console.log(name, value, type, checked);
+    // setAnswersArray(prevAnswers => {
+    // if ()
+    //     return {
+    //         ...prevAnswers,
+    //         [name]: type === "checkbox" ? checked : value
+    //     }
+    // })
+  }
+
+  // create checkAnswer function to update questionCorrect value set in state
 
   return (
     <form className="quiz__container">
