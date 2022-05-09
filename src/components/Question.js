a9a388e924f23d499ddc03cd394615234b7cea29
Sarah Fielding Smith <s.fieldingsmith@gmail.com>
2022-05-09 15:31:40 +0100

    fetch data from https://opentdb.com/

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,42 +1,8 @@
 import React from "react";
-import { nanoid } from "nanoid";
 
 export default function Question(props) {
-  //   create answers array of objects {answer: text, correct: false}
-  const answers = props.incorrectAnswers.map((answer) => {
-    return {
-      questionID: props.questionID,
-      answer: answer,
-      answerID: nanoid(),
-      isCorrect: false,
-      isSelected: false,
-      answerSelected: "",
-    };
-  });
-
-  //   push correct answer into answers array
-  answers.push({
-    questionID: props.questionID,
-    answer: props.correctAnswer,
-    answerID: nanoid(),
-    isCorrect: true,
-    isSelected: false,
-    answerSelected: "",
-  });
-
-  //   randomise array
-  for (let i = answers.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * i);
-    let k = answers[i];
-    answers[i] = answers[j];
-    answers[j] = k;
-  }
-
-  // set answers array in state
-  const [answersArray, setAnswersArray] = React.useState(answers);
-
-  // //   destructure answers array
-  const [answer1, answer2, answer3, answer4] = answers;
+  //   destructure answers array
+  const [answer1, answer2, answer3, answer4] = props.answers;
 
   return (
     <div className="question__container">
@@ -46,7 +12,7 @@ export default function Question(props) {
           type="radio"
           id={answer1.answer}
           name={answer1.questionID}
-          value={answer1.answerID}
+          value="0"
           onChange={props.handleChange}
         />
         <label htmlFor={answer1.answer}>{answer1.answer}</label>
@@ -54,7 +20,7 @@ export default function Question(props) {
           type="radio"
           id={answer2.answer}
           name={answer2.questionID}
-          value={answer2.answerID}
+          value="1"
           onChange={props.handleChange}
         />
         <label htmlFor={answer2.answer}>{answer2.answer}</label>
@@ -62,7 +28,7 @@ export default function Question(props) {
           type="radio"
           id={answer3.answer}
           name={answer3.questionID}
-          value={answer3.answerID}
+          value="2"
           onChange={props.handleChange}
         />
         <label htmlFor={answer3.answer}>{answer3.answer}</label>
@@ -70,7 +36,7 @@ export default function Question(props) {
           type="radio"
           id={answer4.answer}
           name={answer4.questionID}
-          value={answer4.answerID}
+          value="3"
           onChange={props.handleChange}
         />
         <label htmlFor={answer4.answer}>{answer4.answer}</label>
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,29 +1,85 @@
 import React from "react";
 import Question from "./Question";
-import results from "../results";
 
 export default function Quiz() {
-  // map results to new question array
-  let questionArray = results.map((result, index) => {
-    return {
-      question: result.question,
-      questionID: index,
-      questionCorrect: false,
-    };
-  });
+  React.useEffect(() => {
+    fetch(
+      "https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results);
+        let questions = restructureData(data.results);
+        setQuestions(questions);
+      });
+  }, []);
 
   // set questions in state
-  const [questions, setQuestions] = React.useState(questionArray);
+  const [questions, setQuestions] = React.useState([]);
+
+  function restructureData(results) {
+    // map results to new question array
+    let questionsArray = results.map((result, index) => {
+      return {
+        question: result.question,
+        questionID: index,
+        answersArray: [
+          {
+            questionID: index,
+            answer: result.correct_answer,
+            isCorrect: true,
+            isSelected: false,
+          },
+          {
+            questionID: index,
+            answer: result.incorrect_answers[0],
+            isCorrect: false,
+            isSelected: false,
+          },
+          {
+            questionID: index,
+            answer: result.incorrect_answers[1],
+            isCorrect: false,
+            isSelected: false,
+          },
+          {
+            questionID: index,
+            answer: result.incorrect_answers[2],
+            isCorrect: false,
+            isSelected: false,
+          },
+        ],
+      };
+    });
+
+    // for each question, shuffle answersArray
+    questionsArray = questionsArray.map((question) => {
+      let shuffledArray = question.answersArray;
+
+      for (let i = shuffledArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * i);
+        let k = shuffledArray[i];
+        shuffledArray[i] = shuffledArray[j];
+        shuffledArray[j] = k;
+      }
+
+      return {
+        ...question,
+        answersArray: shuffledArray,
+      };
+    });
+
+    return questionsArray;
+  }
 
   // map over results array creating new questionElements
-  const questionElements = results.map((result, index) => {
+  const questionElements = questions.map((question, index) => {
     return (
       <Question
-        question={result.question}
-        correctAnswer={result.correct_answer}
-        incorrectAnswers={result.incorrect_answers}
-        key={index}
+        question={question.question}
         questionID={index}
+        key={index}
+        answers={question.answersArray}
         handleChange={selectAnswer}
       />
     );
@@ -32,11 +88,19 @@ export default function Quiz() {
   // create handleChange function to update isSelected value set in state
   function selectAnswer(event) {
     const { name, value, type, checked } = event.target;
-    console.log(name, value, type, checked);
-    // setAnswersArray(prevAnswers => {
-    // if ()
+    console.log(questions[name].answersArray[value]);
+    // setQuestions(prevQuestions => {
+    //   return prevQuestions.map(question => {
+    //     if (question.questionID === name) {
+    //        if (question.answersArray[value]){
+
+    //        }
+
+    //     }
+    //   })
+
     //     return {
-    //         ...prevAnswers,
+    //         ...prevQuestions,
     //         [name]: type === "checkbox" ? checked : value
     //     }
     // })
